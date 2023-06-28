4ade0eb96bf6e75c6cf98ff1513fb81eddd34cff
Prashidika Tiwari <prashidika2061@gmail.com>
2023-06-28 11:31:20 +0545

    initialized screen settings

--- hangman.py
+++ hangman.py
@@ -1,12 +1,14 @@
 import pygame
 import sys
 
+from settings import Settings
+
 class Hangman:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,700))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.width,self.settings.height))
         pygame.display.set_caption("Hangman")
-        self.bg_color = (177,79,179)
 
     def run_game(self):
         while True:
@@ -16,7 +18,7 @@ class Hangman:
             self.update_screen()
 
     def update_screen(self):
-        self.screen.fill(self.bg_color)
+        self.screen.fill(self.settings.bg_color)
         pygame.display.flip()
 
 if __name__ == '__main__':
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    """ COntains all the settings of the game"""
+    def __init__(self):
+        # Screen settings
+        self.height = 700
+        self.width = 1200
+        self.bg_color = (177,79,179)
